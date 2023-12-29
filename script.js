2aee1326b60a34fe9c755d9d85798e9ca7d96d58
James Senva <jamessenva@gmail.com>
2023-12-30 00:00:41 +0530

    Add features to drag and draw over grid container and choose background color
    1. Add features to drag and draw over grid container
    - Select the pen color from DOM
    - Attach event listener to grid container which handles mousedown event with handleMousedown function
    - handleMousedown function attaches two event listener to grid container
    	1. Handles mousemove event with handleMousemove function
    	2. Handles mouseup event with handleMouseup function
    - handleMousemove function changes the background color of column (grid/pixel) created with JavaScript with the value of pen color
    - handleMouseup function removes the event listener from handleMouseup function itself and handleMousemove function attached to grid container
    - Whole thing works like this
    	1. Mouse is clicked and calls the function to handle mousemove or mouse dragging function and mouseup or mouse button released function
    	2. When the mouse is dragged, it changes the color of pixels in the grid
    
    2. Choose background-color
    - Background color feature has input type of color
    - Add event listener to the bg variable with input event which calls the function to change the background color of grid

--- script.js
+++ script.js
@@ -32,3 +32,36 @@ createGrid();
 gridSlider.addEventListener('input', createGrid);
 
 
+
+//------------- Color options: Pen Color
+const pen = document.querySelector('#pen');
+
+// first the mouse button is clicked over gridbox, it calls the handleMousedown function
+gridContainer.addEventListener('mousedown', handleMousedown);
+
+// handleMousedown function handles two events, mousemove for mouse dragging and mouseup when the mouse button is released
+// also calls their respective callbacks to handle these events
+function handleMousedown() {
+    gridContainer.addEventListener('mousemove', handleMousemove);
+    gridContainer.addEventListener('mouseup', handleMouseup);
+}
+
+// changes the grid color when mouse is dragged
+function handleMousemove(e) {
+    e.target.style.backgroundColor = pen.value;
+}
+
+// when the mouse is released, removes handleMouseup function itself and handleMousemove function which add these functions
+function handleMouseup() {
+    gridContainer.removeEventListener('mouseup', handleMouseup);
+    gridContainer.removeEventListener('mousemove', handleMousemove);
+}
+
+
+//------------- Color options: Background Color
+const bg = document.querySelector('#bg');
+
+// input event updates the background color of gridContainer 
+bg.addEventListener('input', function() {
+  gridContainer.style.backgroundColor = bg.value;
+});
